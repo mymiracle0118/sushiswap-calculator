0550bfb814f5819caaffe372c672f2b945e31b99
mymiracle0118 <noahsflood908@gmail.com>
2024-05-16 23:35:09 +0300

    init commit

---
+++ src/SushiCalculator.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import './IUniswapV2Factory.sol'
+import './IUniswapV2Router02.sol'
+import './IUniswapV2Pair.sol'
+
+contract SushiCalculator {
+
+    constructor(address _factory, address _router) {    
+        
+    }
+
+    function getPair(address tokenA, address tokenB) {
+
+    }
+}
